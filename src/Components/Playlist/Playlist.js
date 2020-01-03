16140520b2987500b449e5d10a814cfc664fc5c5
Chad Saglam <saglam.chd@gmail.com>
2020-01-03 13:45:40 +0100

    Pass down Playlist to TrackList

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Playlist.css';
-// import TrackList from'../TrackList/TrackList';
+import TrackList from'../TrackList/TrackList';
 
 
 class Playlist extends React.Component {
@@ -9,7 +9,7 @@ class Playlist extends React.Component {
       <div className="Playlist">
         <input 
           defaultValue={"New Playlist"} />
-        {/*<TrackList  />*/}
+        <TrackList  tracks={this.props.playlistTracks}/>
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
