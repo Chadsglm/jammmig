4feed81d39253a81a9591fdabfa970d2658284a4
Chad Saglam <saglam.chd@gmail.com>
2020-01-03 15:40:00 +0100

    Obtain a Spotify Access Token

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config({ path: 'variables.env' });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/util/Spotify.js
@@ -0,0 +1,34 @@
+// require('dotenv').config({ path: 'variables.env' });
+
+let accessToken; 
+
+const Spotify =  {
+  getAccessToken() {
+    if(accessToken) {
+      return accessToken
+    }
+
+    // check for access token Match
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if(accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1]
+      const expiresIn = Number(expiresInMatch[1])
+
+      // This clears the parameters, allowing us to grab a new access token when it expires
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${process.env.REDIRECT_URI}`
+      
+      window.location =  accessUrl
+    }
+
+  }
+}
+
+export default Spotify;
+
