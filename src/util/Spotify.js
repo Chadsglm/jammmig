ddd332ed8294bd78805b46de481f3725ae717f2a
Chad Saglam <saglam.chd@gmail.com>
2020-01-05 11:20:05 +0100

    deploying

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,6 +8,7 @@ const Spotify = {
     if(accessToken) {
       return accessToken;
     }
+    
     // check for access token Match
     const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
     const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
